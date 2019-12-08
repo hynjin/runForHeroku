c989e2f86bf89ee0e42f7a211885f1e1a1c18b5d
hynjin <hynjin1004@naver.com>
2019-12-08 21:36:27 +0900

    index.html 수정

--- app.py
+++ app.py
@@ -18,17 +18,19 @@ def info():
 @app.route("/upload", methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        print(request.files['erp'])
         erp = request.files['erp']
-        print(request.files['email'])
         email = request.files['email']
+
         result = taxInvoice.tax(erp,email)
+
         response = Response(
         result.getvalue(),
         mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         content_type='application/octet-stream')
+
         dt = datetime.datetime.now()
         fname = dt.strftime("%m월세금계산서").encode('utf-8').decode('iso-8859-1')
+
         response.headers["Content-Disposition"] = "attachment; filename=%s.xlsx" % fname
         return response
     return render_template('index.html')
