442031ff29db59553ab4492bed2f505f0f298569
hynjin <hynjin1004@naver.com>
2020-05-04 22:43:32 +0900

    file name

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def upload_file():
         dt = datetime.datetime.now()
         month = (int(dt.strftime("%m")) + 11)%12
         month = str(month)+"월세금계산서"
-        fname = month..encode('utf-8').decode('iso-8859-1')
+        fname = month.encode('utf-8').decode('iso-8859-1')
         #dt.strftime("%m월세금계산서").encode('utf-8').decode('iso-8859-1')
 
         response.headers["Content-Disposition"] = "attachment; filename=%s.xlsx" % fname
